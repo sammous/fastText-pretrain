ee025741a8a064cd464dedff615dadd15c9af304
Sami Moustachir <moustachir.sami@gmail.com>
2018-03-26 18:57:05 +0200

    update melt app

--- Makefile
+++ Makefile
@@ -1,7 +1,22 @@
-DOCKER_BUILD_CMD=docker build
+DOCKER_BUILD_CMD=sudo docker build
+DOCKER_MELT_IMAGE=melt
+DOCKER_FASTTEXT_IMAGE=fasttext_image
 
-install:
-	docker volume create --name data
 build:
-	$(DOCKER_BUILD_CMD) -t melt -f DockerfileMElt .
-	$(DOCKER_BUILD_CMD) -t fasttext_pretrain -f Dockerfile .
+	$(DOCKER_BUILD_CMD) -t $(DOCKER_MELT_IMAGE) -f melt/Dockerfile .
+	#$(DOCKER_BUILD_CMD) -t $(DOCKER_FASTTEXT_IMAGE) -f DockerfileFastText .
+
+run:
+	sudo docker run -p 5000:5000 --name melt -v $(pwd)/data:/data $(DOCKER_MELT_IMAGE)
+	echo 'Docker container id for MElt is :'
+	echo '#####'
+	sudo docker ps -aqf "name=melt"
+	echo '#####'
+
+clean:
+	#clean existing containers
+	sudo docker stop melt
+	sudo docker container prune
+	sudo docker rmi melt
+	
+start: clean build run
--- melt/melt_app/src/melt_app.py
+++ melt/melt_app/src/melt_app.py
@@ -10,13 +10,25 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def tokenize_and_post():
     json_data = request.json['data']
-    f = NamedTemporaryFile()
+    f = NamedTemporaryFile(mode='r+w', delete=False)
     f.write(json_data)
     f.read()
     ps = subprocess.Popen(('cat', f.name), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('MElt', '-t'), stdin=ps.stdout.read())
+    output = subprocess.check_output(('MElt', '-t'), stdin=ps.stdout)
     f.close()
     return output
+
+
+@app.route('/tokenize', methods=['POST'])
+def tokenize():
+    json_data = request.json['data']
+    f = NamedTemporaryFile(delete=False)
+    f.write(json_data)
+    ps = subprocess.Popen(('cat', f.name), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('sxpipe-light'), stdin=ps.stdout, shell=True)
+    f.close()
+    return output
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
